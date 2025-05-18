df1de8d6742f9ed2cb21909645811c149643e830
CristianoGheu <120095921+CristianoGheu@users.noreply.github.com>
2025-05-18 23:35:42 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
-    production: true,
-    firebaseConfig: {
-        apiKey: "AIzaSyA4IcdUkAy5oneCiSWJFPF9wouM_iVFxl0",
-        authDomain: "progetto-angular2.firebaseapp.com",
-        projectId: "progetto-angular2",
-        storageBucket: "progetto-angular2.firebasestorage.app",
-        messagingSenderId: "835862231736",
-        appId: "1:835862231736:web:71cecce55d0109cf25524d"
-    }
+    production: true
   };
   
